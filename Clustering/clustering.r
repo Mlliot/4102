3ffdfc867b5cd61fd963e3686a4107f7020e3f47
Carson Bavery <ctrackb31@gmail.com>
2024-11-23 21:42:31 -0500

    Created a linear regression program that takes in song data and plots it against the linear regression that is calculated for songs over 100 million streams

--- Clustering/clustering.r
+++ Clustering/clustering.r
@@ -14,9 +14,9 @@ kmeans_result <- kmeans(clustering_data, centers = 3)
 
 uploaded_data$Cluster <- as.factor(kmeans_result$cluster)
 
-cluster_plot <- ggplot(uploaded_data, aes(x = BALANCE, y = PURCHASES, color = Cluster)) +
+cluster_plot <- ggplot(uploaded_data, aes(x = CREDIT_LIMIT, y = PURCHASES, color = Cluster)) +
   geom_point(size = 3) +
-  labs(title = "K-Means Clustering", x = "Balance", y = "Purchases") +
+  labs(title = "K-Means Clustering", x = "Credit Limit", y = "Purchases") +
   theme_minimal()
 
 print(cluster_plot)
